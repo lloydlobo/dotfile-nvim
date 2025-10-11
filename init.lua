2776f373d540f3ad64f1a7751293a20943a00bec
Lloyd Lobo <lloydlobo4@gmail.com>
2025-10-11 18:53:52 +0530

    feat: enhance minimal ui with modern api

--- init.lua
+++ init.lua
@@ -1,25 +1,49 @@
 local opt, map = vim.opt, vim.keymap.set
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-opt.clipboard = "unnamedplus"
-opt.ignorecase = true
-opt.laststatus = 0
 opt.undofile = true
-
+opt.clipboard = "unnamedplus"
+opt.ignorecase, opt.smartcase = true, true
+opt.laststatus = 0
+opt.ruler, opt.showcmd, opt.showmode = false, false, false
+opt.cursorline, opt.wrap, opt.breakindent = false, false, true
+opt.signcolumn = "auto:4-9" -- always show, exactly 4 columns (or yes:4) (max 9)
+opt.termguicolors = true
 opt.expandtab = true
-opt.shiftwidth = 4
-opt.softtabstop = opt.shiftwidth:get()
-opt.tabstop = 4
+opt.tabstop, opt.shiftwidth = 4, 4
+opt.softtabstop = -1 -- same as opt.shiftwidth:get()
+opt.scrolloff = 10
+opt.splitright, opt.splitbelow = true, true
+opt.inccommand = "split" -- show effects of |:substitute|, |:smagic|, |:snomagic| and user commands with |:command-preview|
 
--- • See also: https://www.masswerk.at/nowgobang/2019/dec-crt-typography
--- • colorscheme {lunaperche,retrobox,wildcharm,zaibatsu} | ... guibg=#231e1b
-vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
+vim.cmd.syntax("off")
+vim.cmd.colorscheme(({ "default", "retrobox", "wildcharm" })[3])
+vim.api.nvim_set_hl(
+    0,
+    "Normal",
+    ({
+        { bg = "#111111", fg = "#5fd7ff" }, --[[VT100 soft cyan]]
+        { bg = "NONE", fg = "#82def7" }, --[[VT100 default cyan]]
+        { bg = "#282828", fg = "#d78700" }, --[[VT220 yellow]]
+    })[1]
+)
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlighting" })
-map("n", "<space>y", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Yank file path to clipboard" })
-map("n", "<space>c", function() -- alternatively, use `:vnew | read !cmd` and manually close buffer
+map("n", "<leader>d", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
+map("n", "<leader>m", "<cmd>marks<CR>", { desc = "List all marks/bookmarks" })
+map("n", "<leader>M", "<cmd>delmarks!<CR>", { desc = "Delete all lowercase marks/bookmarks" })
+map("n", "<leader>y", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Yank file path to clipboard" })
+map("n", "<leader>c", function() -- alternatively, use `:vnew | read !cmd` and manually close the buffer
     vim.ui.input({ prompt = "$ " }, function(cmd)
         if not cmd or cmd == "" then return end
         vim.cmd("noswapfile vnew | setlocal buftype=nofile bufhidden=wipe filetype=sh scrolloff=0 nowrap")
         vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(cmd))
     end)
 end, { desc = "Run command in scratch buffer" })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function() vim.highlight.on_yank() end,
+    desc = "Highlight when yanking text",
+})
--- readme.md
+++ readme.md
@@ -1,10 +1,89 @@
 # nvim
 
-## references
+a minimal, distraction-free Neovim configuration without plugins.
 
-- [why I got rid of all my neovim plugins — yobibyte](https://yobibyte.github.io/vim.html)
+---
 
 ## notes
 
 - Too spartan?  ->  `alias vim-kickstart='NVIM_APPNAME="nvim-kickstart" nvim'`
-- Format me     ->  $ stylua init.lua
+- Format code?  ->  `stylua init.lua`
+
+---
+
+## keybindings
+
+| Key | Action |
+|-----|--------|
+| `<Esc>` | Clear search highlighting |
+| `<leader>d` | Delete buffer |
+| `<leader>m` | List marks/bookmarks |
+| `<leader>M` | Delete all marks |
+| `<leader>y` | Yank file path to clipboard |
+| `<leader>c` | Run shell command in scratch buffer |
+
+### Marks (Built-in Bookmarks)
+
+- `m{a-z}` — Set local mark
+- `m{A-Z}` — Set global mark (across files)
+- `'{a-z}` — Jump to mark (line)
+- `` `{a-z}`` — Jump to mark (exact position)
+
+---
+
+## default mappings
+
+```txt
+DEFAULT MAPPINGS
+                                                        *default-mappings*
+Nvim creates the following default mappings at |startup|. You can disable any
+of these in your config by simply removing the mapping, e.g. ":unmap Y". If
+you never want any default mappings, call |:mapclear| early in your config.
+
+- Y |Y-default|
+- <C-U> |i_CTRL-U-default|
+- <C-W> |i_CTRL-W-default|
+- <C-L> |CTRL-L-default|
+- & |&-default|
+- Q |v_Q-default|
+- @ |v_@-default|
+- # |v_#-default|
+- * |v_star-default|
+- gc |gc-default| |v_gc-default| |o_gc-default|
+- gcc |gcc-default|
+- gr prefix |gr-default|
+  - |grn|
+  - |grr|
+  - |gra|
+  - |gri|
+  - |grt|
+  - |gO|
+- <C-S> |i_CTRL-S|
+- ]d |]d-default|
+- [d |[d-default|
+- [D |[D-default|
+- ]D |]D-default|
+- <C-W>d |CTRL-W_d-default|
+- |[q|, |]q|, |[Q|, |]Q|, |[CTRL-Q|, |]CTRL-Q|
+- |[l|, |]l|, |[L|, |]L|, |[CTRL-L|, |]CTRL-L|
+- |[t|, |]t|, |[T|, |]T|, |[CTRL-T|, |]CTRL-T|
+- |[a|, |]a|, |[A|, |]A|
+- |[b|, |]b|, |[B|, |]B|
+- |[<Space>|, |]<Space>|
+- Nvim LSP client defaults |lsp-defaults|
+  - K |K-lsp-default|
+
+ vim:tw=78:ts=8:sw=2:et:ft=help:norl:
+```
+
+---
+
+## references
+
+- [why I got rid of all my neovim plugins — yobibyte](https://yobibyte.github.io/vim.html)
+
+    - [yobibyte/yobitools/dotfiles/init.lua](https://github.com/yobibyte/yobitools/blob/main/dotfiles/init.lua)
+    - [wdomitrz/kitty_gruvbox_theme](https://raw.githubusercontent.com/wdomitrz/kitty_gruvbox_theme/refs/heads/master/gruvbox_light.conf)
+
+- [Raster CRT Typography — masswerk](https://www.masswerk.at/nowgobang/2019/dec-crt-typography)
+
